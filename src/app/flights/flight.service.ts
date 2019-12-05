b3ae46799ca54cdb92142977c0d795f1f992b630
Peiran Zeng <pzeng2@hawk.iit.edu>
2019-12-05 18:47:43 -0500

    have error

--- src/app/flights/flight.service.ts
+++ src/app/flights/flight.service.ts
@@ -21,22 +21,24 @@ export class FlightService {
     this.originCity=searchCriteria['origin'];
     this.destinationCity=searchCriteria['destination'];
     this.travelDate=searchCriteria['date'];
+
+    console.log(this.flightNumber,this.originCity,this.destinationCity,this.travelDate);
    
-  if(this.originCity !=="" && this.destinationCity !=="" && this.flightNumber !=="" && this.travelDate !==""){
+  if((this.originCity !==""  || this.originCity !==null) && (this.destinationCity !==null || this.destinationCity !=="") && (this.flightNumber !==null || this.flightNumber !== "") && (this.travelDate !==null || this.travelDate != "")){
     console.log('one');
     return this.http.get(`http://localhost:8080/flights/${this.flightNumber}?departure=${this.travelDate}&origin=${this.originCity}&destination=${this.destinationCity}`,{headers:this.headers}).pipe(
       map(flights => flights),
       catchError(err => throwError(err))
     );
   }
-  else if(this.flightNumber===""){
+  else if(this.flightNumber===null || this.flightNumber===""){
     console.log('two');
     return this.http.get(`http://localhost:8080/flights/?departure=${this.travelDate}&origin=${this.originCity}&destination=${this.destinationCity}`,{headers:this.headers}).pipe(
       map(flights => flights),
       catchError(err => throwError(err))
     );
   }
-  else if(this.originCity ==="" && this.destinationCity ===""){
+  else if((this.originCity ===null || this.originCity === "") && (this.destinationCity ===null || this.destinationCity === "")){
     console.log('three');
   return this.http.get(`http://localhost:8080/flights/${this.flightNumber}?departure=${this.travelDate}`,{headers:this.headers}).pipe(
     map(flights => flights),
